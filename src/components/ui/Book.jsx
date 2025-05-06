f27eb7000abd230775d4230f1ac54c5ee76a909a
Nick <nickjinks60@gmail.com>
2025-05-05 22:08:00 -0400

    seeing if package.json pushes

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,42 +1,26 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import Price from "./Price";
 
 const Book = ({ book }) => {
-    const rating = 4;
   return (
-  <div className="book">
-                      <Link to="">
-                          <figure className="book__img--wrapper">
-                             <img src={book.url} alt="" className="book__img" />
-                          </figure>
-                      </Link>
-                      <div className="book__title">
-                          <Link to="/" className="book__title--link">
-                         {book.title}</Link>
-                      </div>
-                      <div className="book__ratings">
-                         {
-                            new Array(Math.floor(book.rating)).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index} /> )
-                         }
-                         {
-                           !Number.isInteger(book.rating) && <FontAwesomeIcon icon="star-half-alt"  />
-                         }
-                      </div>
-                      <div className="book__price">
-                        {
-                         book.salePrice
-                         ? ( <>
-                         <span className="book__price--normal">${book.originalPrice.toFixed(2)}</span>
-                          ${book.salePrice.toFixed(2)}</> 
-                         ) : (
-                            <>${ book.originalPrice.toFixed(2) }</>
-                          )
-                        }
-                        
-                      </div>
-                  </div>
-  )
-}
+    <div className="book">
+      <Link to={`/books/${book.id}`}>
+        <figure className="book__img--wrapper">
+          <img src={book.url} alt="" className="book__img" />
+        </figure>
+      </Link>
+      <div className="book__title">
+        <Link to={`/books/${book.id}`}className="book__title--link">
+          {book.title}
+        </Link>
+        <Rating rating={book.rating} />
+      </div>
+      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+    </div>
+  );
+};
 
 export default Book;
